77a24b5a241e24edb86d269b070d9ac31b333782
Nikolay Kostov <admin@nikolay.it>
2014-11-07 17:14:35 +0200

    Bootstrap theme changed to cosmo Removed modernizr

--- Source/Web/ForumSystem.Web/App_Start/BundleConfig.cs
+++ Source/Web/ForumSystem.Web/App_Start/BundleConfig.cs
@@ -14,17 +14,12 @@ namespace ForumSystem.Web
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.cosmo.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
