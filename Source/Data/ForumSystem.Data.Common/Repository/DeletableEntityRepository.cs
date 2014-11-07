a0d1aeea9d3c28ad7c88ba4f5bfa1f56f9f45641
Nikolay Kostov <admin@nikolay.it>
2014-11-07 16:55:03 +0200

    Implemented repository interfaces

---
+++ Source/Data/ForumSystem.Data.Common/Repository/DeletableEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace ForumSystem.Data.Common.Repository
+{
+    using System.Linq;
+
+    using ForumSystem.Data.Common.Models;
+    using System.Data.Entity;
+
+    public class DeletableEntityRepository<T> : GenericRepository<T>, IDeletableEntityRepository<T>
+        where T : class, IDeletableEntity
+    {
+        public DeletableEntityRepository(DbContext context)
+            : base(context)
+        {
+        }
+
+        public override IQueryable<T> All()
+        {
+            return base.All().Where(x => !x.IsDeleted);
+        }
+
+        public IQueryable<T> AllWithDeleted()
+        {
+            return base.All();
+        }
+    }
+}
